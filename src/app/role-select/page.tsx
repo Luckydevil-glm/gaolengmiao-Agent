09e0e8dac96132032ca87595abaae4c45737bf5e
Luckydevil-glm <xcdongfan@126.com>
2026-03-24 03:48:17 +0800

    Fix: add Link import to role-select

--- src/app/role-select/page.tsx
+++ src/app/role-select/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { User, Store, Sparkles, ArrowRight, ArrowLeft } from 'lucide-react'
 import { useUserStore } from '@/stores/userStore'
